a4531a8b4f88625f23f843aab4d68c19a2af1380
sahu0957 <sahu0957@colorado.edu>
2023-02-14 20:58:26 -0700

    added featurecounts bash wrappers

---
+++ scripts/featurecounts_GRO_SE.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+#SBATCH --job-name=featurecounts # Job name
+#SBATCH --mail-type=ALL # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=user@email.edu # Where to send mail
+#SBATCH --nodes=1
+#SBATCH --ntasks=16 # Number of CPU (processer cores i.e. tasks)
+#SBATCH --time=00:30:00 # Time limit hrs:min:sec
+#SBATCH -p short
+#SBATCH --mem=4gb # Memory limit
+#SBATCH --output=/path/to/e_o/%x_%j.out
+#SBATCH --error=/path/to/e_o/%x_%j.err
+
+module load python/3.6.3
+module load subread
+
+while test $# -gt 0; do
+           case "$1" in
+                -h|--help)
+                    echo "Uses FeatureCounts to count single-end reads over input regions"
+                    echo " "
+                    echo "options:"
+                    echo "-h, --help, show brief help"
+                    echo "-b|--bamdir, BAM directory file"
+                    echo "-r|--refsaf, SAF file to count over"
+                    echo "-n|--name, BAM name"
+                    echo "-o|--outfile, output file for counts" 
+                    exit 0
+                    ;;
+                -b|--bamdir)
+                    shift
+                    BAMDIR=$1
+                    shift
+                    ;;
+                -n|--name)
+                    shift
+                    NAME=$1
+                    shift
+                    ;;
+                -r|--refsaf)
+                    shift
+                    REFSAF=$1
+                    shift
+                    ;;
+                -o|--outfile)
+                    shift
+                    OUTFILE=$1
+                    shift
+                    ;;
+                *)
+                   echo "$1 is not a recognized flag!"
+                   return 1;
+                   ;;
+          esac
+  done
+
+printf "\nRun on: $(hostname)"
+printf "\nRun from: $(pwd)"
+printf "\nScript: $0\n"
+date
+
+printf "\nYou've requested $SLURM_CPUS_ON_NODE core(s).\n"
+
+declare -a BamFiles
+for file in `ls ${BAMDIR}/${NAME}*bam`
+do
+    BamFiles=("${BamFiles[@]}" "$file")
+done
+
+function logr {
+    echo "[""$(date -d@$SECONDS -u +%H:%M:%S)""]: $*"
+}
+
+#logr "Parsed Params: ""$regionFile"
+
+logr "Starting Analysis"
+
+NUM_CORES=8
+safFile=$REFSAF
+countsOut=${OUTFILE}.sense.txt
+
+featureCounts \
+                -T "$NUM_CORES" \
+                -s 1 \
+		-f \
+                --fracOverlap 0.51 \
+                -O \
+                -F 'SAF' \
+                -a "$safFile" \
+                -o "$countsOut" \
+                ${BamFiles[@]} 
+
+
+countsOut=${OUTFILE}.antisense.txt
+featureCounts \
+                -T "$NUM_CORES" \
+                -s 2 \
+		-f \
+                --fracOverlap 0.51 \
+                -O \
+                -F 'SAF' \
+                -a "$safFile" \
+                -o "$countsOut" \
+                ${BamFiles[@]}
+
+countsOut=${OUTFILE}.unstranded.txt
+featureCounts \
+                -T "$NUM_CORES" \
+                -s 0 \
+                --fracOverlap 0.51 \
+                -F 'SAF' \
+                -a "$safFile" \
+                -o "$countsOut" \
+                ${BamFiles[@]} 
